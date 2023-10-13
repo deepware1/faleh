372b683d3545c60dab225701ac8081867ed59b90
Mohamed Samir <gm.mohamedsamir@gmail.com>
2023-10-13 04:42:39 +0300

    fix image

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -5,7 +5,7 @@
         <div class="grid grid-cols-4 gap-3">
             @foreach ($categories as $category)
                 <div class="relative rounded-sm overflow-hidden group">
-                    <img src="{{ $category->getFirstMediaUrl('categories') }}" alt="{{ $category->title }}" class="w-full">
+                    <img src="{{ $category->image() }}" alt="{{ $category->title }}" class="w-full">
                     <a href="{{ route('subcategories', $category->slug) }}"
                         class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">
                         {{ $category->title }}
