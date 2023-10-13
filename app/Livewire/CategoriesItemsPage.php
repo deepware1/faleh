8362763ec7d07b777a07a194f59c12b030610bd0
Mohamed Samir <gm.mohamedsamir@gmail.com>
2023-10-13 21:01:04 +0300

    add pagination

--- app/Livewire/CategoriesItemsPage.php
+++ app/Livewire/CategoriesItemsPage.php
@@ -2,21 +2,49 @@
 
 namespace App\Livewire;
 
+use Livewire\Component;
 use App\Models\Category;
+use Livewire\WithPagination;
 
-class CategoriesItemsPage extends  BasePage
+class CategoriesItemsPage extends Component
 {
+    use WithPagination;
+
     protected string $viewPath = "livewire.categories-items-page";
     protected string $pageTitle = "Items";
 
     // page data
     public Category $category;
-    public $items;
 
     public function mount(Category $category)
     {
         $this->category = $category;
-        $this->items = $category->items()->isPublished()->get();
+    }
+
+    public function render()
+    {
+        return view($this->viewPath, [
+            "items" => $this->category->items()->isPublished()->simplePaginate(2),
+        ]);
+    }
+
+    public function previousPage($pageName = 'page')
+    {
+        $page = max(($this->paginators[$pageName] ?? 1) - 1, 1);
+
+        return redirect()->route("subcategories.items", [
+            "category" => $this->category,
+            "page" => $page
+        ]);
+    }
+
+    public function nextPage($pageName = 'page')
+    {
+        $page = ($this->paginators[$pageName] ?? 1) + 1;
+
+        return redirect()->route("subcategories.items", [
+            "category" => $this->category,
+            "page" => $page
+        ]);
     }
 }
-
--- resources/views/livewire/categories-items-page.blade.php
+++ resources/views/livewire/categories-items-page.blade.php
@@ -1,4 +1,4 @@
-<div class="container mx-auto">
+<div class="container mx-auto py-16">
     <div class="container pb-16">
         <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">Items</h2>
         <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
@@ -6,6 +6,9 @@
                 <livewire:shared.item :item="$item" />
             @endforeach
         </div>
-    
+
+        <div class="max-w-sm mx-auto mt-4">
+            {{ $items->links() }}
+        </div>
     </div>
 </div>
